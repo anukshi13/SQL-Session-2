8161e61158e532683f837e2ec951517e066046cd
anukshi13 Mittal <anukshi.mittal@kellogg.northwestern.edu>
2025-01-14 21:27:31 +0000

    Completetd Lab 2

--- lab-2-1.sql
+++ lab-2-1.sql
@@ -14,6 +14,6 @@
 -- | 2019 | Minnesota Twins        | 101  |
 -- | 2019 | New York Yankees       | 103  |
 -- +------+------------------------+------+
-SELECT year, name FROM teams
+SELECT year, name , wins FROM teams
 WHERE wins >100 ORDER BY year ASC
 
--- lab-2-3.sql
+++ lab-2-3.sql
@@ -6,7 +6,6 @@
 -- | 2017 | Chicago White Sox |
 -- +------+-------------------+
 SELECT name, year FROM teams
-ORDER BY year DESC 
-LIMIT 1 
-WHERE park = "U.S> Cellular Field"
-
+WHERE park = "U.S. Cellular Field"
+ORDER BY year DESC
+LIMIT 1
