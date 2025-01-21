a346c302e0cb7b052fa318d04abe0c7616bc7c97
anukshi13 Mittal <anukshi.mittal@kellogg.northwestern.edu>
2025-01-21 20:13:59 +0000

    school changes

--- school-2-drop-and-create.sql
+++ school-2-drop-and-create.sql
@@ -1,3 +1,10 @@
 -- Drop the students table
 
 -- Recreate it
+DROP TABLE IF EXISTS students;
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
